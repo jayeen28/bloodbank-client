a7422ff29207b6b6541d0c14c3f8352fce4fa7c4
Jayeen28 <jayeen6783@gmail.com>
2022-06-07 23:01:54 +0600

    signup page design and functionality done

--- src/Components/Main/Auth/Signup/Signup.js
+++ src/Components/Main/Auth/Signup/Signup.js
@@ -1,22 +1,29 @@
+import { Button, Container, TextField } from "@mui/material";
 import { useForm } from "react-hook-form";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useAlert } from "../../../../Hooks/useAlert";
 import { useManageUsers } from "../../../../Hooks/useManageUsers";
+import './Signup.css';
 
 export const Signup = () => {
     const { showMessage } = useAlert();
     const { createUser } = useManageUsers();
+    const navigate = useNavigate();
     const { handleSubmit, register, reset } = useForm();
 
     const handleSignup = data => {
+        //remvoe empty fields
+        const notFilled = Object.keys(data).find(key => data[key] === '');
+        if (notFilled) return showMessage('Please fill all fields.', 'error');
         if (data.password.length > 6) {
             createUser(data)
                 .then(() => {
-                    showMessage('Successfully registered. Please wait till approval.', 'success');
+                    showMessage('Successfully registered.', 'success');
+                    navigate('/login');
                     reset();
                 })
-                .catch(() => {
-                    showMessage('Something went wrong while sign up', 'error');
+                .catch(({ response: { data } }) => {
+                    showMessage(data, 'error');
                 })
         }
         else {
@@ -25,15 +32,78 @@ export const Signup = () => {
     }
 
     return (
-        <div>
-            <h1>Sign up</h1>
-            <form onSubmit={handleSubmit(handleSignup)}>
-                <input type="text" name="name" {...register("name")} required />
-                <input type="email" name="email" {...register("email")} required />
-                <input type="password" name="password"  {...register("password")} required />
-                <input type="submit" />
-            </form>
-            <p>Already have an account? Please <Link to='/login'>Login</Link></p>
-        </div>
+        <Container>
+            <section className="signup-section">
+                <div className="signup-section-head">
+                    <div className="pageHead">
+                        <h1 style={{ textAlign: 'center' }}>Signup</h1>
+                    </div>
+                </div>
+                <div className="signup-section-content">
+                    <form onSubmit={handleSubmit(handleSignup)} className="signup-section-content-form">
+                        <div className="signup-section-content-form-input">
+                            <TextField
+                                label="Name"
+                                variant="standard"
+                                name="name"
+                                {...register('name')}
+                                autoComplete="name"
+                                autoFocus
+                            />
+                        </div>
+                        <div className="signup-section-content-form-input">
+                            <TextField
+                                label="Email"
+                                variant="standard"
+                                name="email"
+                                {...register('email')}
+                                autoComplete="email"
+                            />
+                        </div>
+                        <div className="signup-section-content-form-input">
+                            <TextField
+                                label="Password"
+                                variant="standard"
+                                name="password"
+                                {...register('password')}
+                                autoComplete="current-password"
+                                type="password"
+                            />
+                        </div>
+                        <div className="signup-section-content-form-input">
+                            <TextField
+                                label="Phone"
+                                variant="standard"
+                                name="phone"
+                                {...register('phone')}
+                                defaultValue="01"
+                                type="text"
+                            />
+                        </div>
+                        <div className="signup-section-content-form-input">
+                            <TextField
+                                label="Address"
+                                variant="standard"
+                                name="address"
+                                {...register('address')}
+                                type="text"
+                            />
+                        </div>
+                        <div className="signup-section-content-form-input">
+                            <TextField
+                                label="Blood Group"
+                                variant="standard"
+                                name="bloodGroup"
+                                {...register('bloodGroup')}
+                                type="text"
+                            />
+                        </div>
+                        <div className="signup-section-content-form-input">
+                            <Button type="submit" variant="contained" color="primary">Signup</Button>
+                        </div>
+                    </form>
+                </div>
+            </section >
+        </Container >
     )
 };
