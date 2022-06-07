a508dd0ce6ef4804f2d34b73c0c372a477c79769
Jayeen28 <jayeen6783@gmail.com>
2022-06-07 22:16:37 +0600

    login page design done

--- src/Components/Main/Auth/Login/Login.js
+++ src/Components/Main/Auth/Login/Login.js
@@ -1,3 +1,4 @@
+import { Button, Container, TextField } from "@mui/material";
 import cryptoJs from "crypto-js";
 import Cookies from 'js-cookie';
 import { useForm } from "react-hook-form";
@@ -5,6 +6,7 @@ import { Link, useNavigate } from "react-router-dom";
 import useAuth from "../../../../Context/ContextHooks/useAuth";
 import { useAlert } from "../../../../Hooks/useAlert";
 import { useManageUsers } from "../../../../Hooks/useManageUsers";
+import './Login.css';
 
 export const Login = () => {
     const { register, handleSubmit } = useForm();
@@ -26,7 +28,8 @@ export const Login = () => {
         //handle user login function
         userLogin(data)
             .then(({ data }) => {
-                manageLogedIndata(data)
+                manageLogedIndata(data);
+                navigate(-1);
             })
             .catch(() => {
                 setisLoading(false);
@@ -34,19 +37,45 @@ export const Login = () => {
             })
             .finally(() => {
                 setisLoading(false);
-                // redirect user to where he has came from
-                navigate('/trade');
             })
     }
     return (
-        <div>
-            <h1>Login</h1>
-            <form onSubmit={handleSubmit(handleLogin)}>
-                <input type="email" name="email" {...register("email")} required />
-                <input type="password" name="password"  {...register("password")} required />
-                <input type="submit" />
-            </form>
-            <p>Don't have any account? Please <Link to='/signup'>Signup</Link></p>
-        </div>
+        <Container>
+            <section className="login-section">
+                <div className="login-section-head">
+                    <div className="pageHead">
+                        <h1 style={{ textAlign: 'center' }}>Login</h1>
+                    </div>
+                </div>
+                <div className="login-section-content">
+                    <form onSubmit={handleSubmit(handleLogin)} className="login-section-content-form">
+                        <div className="login-section-content-form-input">
+                            <TextField
+                                label="Email"
+                                variant="standard"
+                                name="email"
+                                {...register('email')}
+                                autoComplete="email"
+                                autoFocus
+                            />
+                        </div>
+                        <div className="login-section-content-form-input">
+                            <TextField
+                                label="Password"
+                                variant="standard"
+                                name="password"
+                                {...register('password')}
+                                autoComplete="password"
+                                type="password"
+                            />
+                        </div>
+                        <span>Don't have an account? Please <Link to="/signup">signup</Link>.</span>
+                        <div className="login-section-content-form-input">
+                            <Button type="submit" variant="contained" color="primary">Login</Button>
+                        </div>
+                    </form>
+                </div>
+            </section>
+        </Container>
     )
 }
