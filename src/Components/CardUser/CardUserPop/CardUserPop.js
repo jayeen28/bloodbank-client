c0dc42db46a3b0f4425e7bbd43910f46e36a8a84
Jayeen28 <jayeen6783@gmail.com>
2022-07-05 17:45:09 +0600

    card user pop update

--- src/Components/CardUser/CardUserPop/CardUserPop.js
+++ src/Components/CardUser/CardUserPop/CardUserPop.js
@@ -1,15 +1,18 @@
 export const CardUserPop = ({ user }) => {
     return (
         <div className="card-user-pop-wrapper">
-            <div
-                className='card-user-profile-img'
+            <div className='card-user-profile-img'
                 style={{ border: `2px solid ${user.donating ? 'red' : 'white'}` }}
             >
                 <img src={user.avatar} alt="user image" />
             </div>
             <div className='card-user-profile-name'>
                 <h4>{user.name}</h4>
-                <span>{user.bloodGroup}</span>
+                <span>Blood group: {user.bloodGroup}</span>
+            </div>
+            <div className="card-user-pop-body">
+                <p>Address: {user.address}</p>
+                <p>Phone: {user.phone}</p>
             </div>
         </div>
     )
